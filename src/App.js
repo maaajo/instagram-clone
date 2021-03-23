ecec368ff1e0e52310ca5cc465dc1f32d9115ce9
Mateusz Majchrzak <matemajch@gmail.com>
2021-03-23 10:14:18 +0100

    login page done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
+
+const Login = lazy(() => import('./pages/login'));
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello1</p>
-    </div>
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path={ROUTES.LOGIN} component={Login}></Route>
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true
+  },
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        white: '#ffffff',
+        blue: {
+          medium: '#005c98'
+        },
+        black: {
+          light: '#005c98',
+          faded: '#00000059'
+        },
+        gray: {
+          base: '#616161',
+          background: '#fafafa',
+          primary: '#dbdbdb'
+        },
+        red: {
+          primary: '#ed4956'
+        }
+      },
+      fill: theme => ({
+        red: theme('colors.red.primary')
+      })
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [],
+  future: {
+    removeDeprecatedGapUtilities: true
+  }
+};
