8b43522260c850da88317c8de2ec82fcc821ce52
Mateusz Majchrzak <matemajch@gmail.com>
2021-03-24 09:51:28 +0100

    add docuemnt title change to not found page

--- src/pages/notfound.js
+++ src/pages/notfound.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const NotFound = () => (
-  <div className="bg-gray-background">
-    <div className="mx-auto max-w-screen-lg">
-      <p className="text-center text-2xl">Not Found!</p>
+const NotFound = () => {
+  useEffect(() => {
+    document.title = 'Not Found!';
+  }, []);
+
+  return (
+    <div className="bg-gray-background">
+      <div className="mx-auto max-w-screen-lg">
+        <p className="text-center text-2xl">Not Found!</p>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default NotFound;
