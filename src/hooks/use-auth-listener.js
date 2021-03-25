f7d6f358edc13557362fe169a84f150f3384f234
Mateusz Majchrzak <matemajch@gmail.com>
2021-03-25 09:40:28 +0100

    hook completed

--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -1,4 +1,25 @@
 import React, { useState, useEffect, useContext } from 'react';
+import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from 'react-dom/cjs/react-dom.development';
 import FirebaseContext from '../context/firebase';
 
-export default function useAuthListener() {}
+export default function useAuthListener() {
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem('authUser'))
+  );
+
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const listener = firebase.auth().onAuthStateChanged(authUser => {
+      if (authUser) {
+        localStorage.setItem('authUser', JSON.stringify(authUser));
+        setUser(authUser);
+      } else {
+        localStorage.removeItem('authUser');
+        setUser(null);
+      }
+    });
+  }, [firebase]);
+
+  return { user };
+}
